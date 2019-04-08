5359651d956c3cf6444b849529187eea5a833fe1
Honza Bittner <git@honzabittner.cz>
2019-04-08 14:03:50 +0200

    finish 3rd lab

---
+++ homeworks/lab-3/README.md
@@ -0,0 +1,7 @@
+# Rozdíl mezi ECB a CBC
+
+**ECB** šifruje otevřené bloky na stejné šifrové bloky, čímž je výsledná podoba ve stejném tvaru a lze odvodit, například u obrázkových dat, původní zprávu. U obrázkových dat tak dochází jen k změně barvy daných bloků.
+
+**СBC** oproti tomu šifruje otevřené bloky spolu s předešlým šifrovým blokem, čímž se vytváří zdánlivě náhodný šifrový blok a nelze, např. u obrázkových dat, odvodit původní zprávu.
+
+
--- homeworks/lab-3/task.cpp
+++ homeworks/lab-3/task.cpp
@@ -1,5 +1,9 @@
 #include <openssl/evp.h>
 #include <iostream>
+#include <fstream>
+
+#define KEY_VALUE "Muj klic"
+#define IV_VALUE "inicial. vektor"
 
 enum class TASK {
     ENCRYPT,
@@ -13,9 +17,20 @@ enum class MODE {
     NONE
 };
 
+enum class METHOD {
+    ENCRYPT,
+    DECRYPT
+};
+
+std::string outputFileName;
+
 std::string removeExtension(const std::string &fileName);
 
 std::string getModeName(MODE mode);
+
+bool isFileBMP(char a, char b);
+
+int process(std::fstream &inputFile, std::fstream &outputFile, MODE mode, METHOD method);
 
 int main(int argc, char *argv[]) {
     // Check number of parameters.
@@ -23,6 +38,8 @@ int main(int argc, char *argv[]) {
         std::cout << "Wrong number of arguments." << std::endl;
         return 1;
     }
+
+    OpenSSL_add_all_ciphers();
 
     std::string taskData = argv[1];
     std::string modeData = argv[2];
@@ -37,9 +54,9 @@ int main(int argc, char *argv[]) {
         task = TASK::DECRYPT;
     }
 
-    if (modeData == "ecb") {
+    if (modeData == getModeName(MODE::ECB)) {
         mode = MODE::ECB;
-    } else if (modeData == "cbc") {
+    } else if (modeData == getModeName(MODE::CBC)) {
         mode = MODE::CBC;
     }
 
@@ -49,40 +66,58 @@ int main(int argc, char *argv[]) {
         return 2;
     }
 
-    // Read the file. r = read, b = binary
-    FILE *file = fopen(sourceFile.c_str(), "rb");
-    if (!file) {
-        std::cout << "Couldn't open the file." << std::endl;
+    // Read the input file.
+    std::fstream inputFile;
+    inputFile.open(sourceFile, std::ifstream::in | std::ifstream::binary);
+    if (!inputFile.is_open()) {
+        std::cout << "Couldn't open the inputFile." << std::endl;
         return 3;
     }
 
-    // encrypt
+    std::fstream outputFile;
+
+    // process
     if (task == TASK::ENCRYPT) {
-        FILE *outputFile = fopen((removeExtension(sourceFile) + "_" + getModeName(mode) + ".bmp").c_str(), "wb");
-        if (!outputFile) {
-            std::cout << "Couldn't open the output file." << std::endl;
-            fclose(file);
+        outputFileName = removeExtension(sourceFile) + "_" + getModeName(mode) + ".bmp";
+        outputFile.open(outputFileName,
+                        std::ifstream::out | std::ifstream::binary);
+        if (!outputFile.is_open()) {
+            std::cout << "Couldn't open the output inputFile." << std::endl;
+            inputFile.close();
+            std::remove(outputFileName.c_str());
             return 4;
         }
 
-        // TODO
-
-        fclose(outputFile);
+        if (process(inputFile, outputFile, mode, METHOD::ENCRYPT) < 0) {
+            std::cout << "Error while encrypting." << std::endl;
+            inputFile.close();
+            outputFile.close();
+            std::remove(outputFileName.c_str());
+            return 5;
+        }
     } // decrypt
     else {
-        FILE *outputFile = fopen((removeExtension(sourceFile) + "_" + getModeName(mode) + ".bmp").c_str(), "wb");
-        if (!outputFile) {
-            std::cout << "Couldn't open the output file." << std::endl;
-            fclose(file);
+        std::fstream outputFile;
+        outputFileName = removeExtension(sourceFile) + "_dec.bmp";
+        outputFile.open(outputFileName, std::ifstream::out | std::ifstream::binary);
+        if (!outputFile.is_open()) {
+            std::cout << "Couldn't open the output inputFile." << std::endl;
+            inputFile.close();
+            std::remove(outputFileName.c_str());
             return 4;
         }
 
-        // TODO
-
-        fclose(outputFile);
+        if (process(inputFile, outputFile, mode, METHOD::DECRYPT) < 0) {
+            std::cout << "Error while decrypting." << std::endl;
+            inputFile.close();
+            outputFile.close();
+            std::remove(outputFileName.c_str());
+            return 5;
+        }
     }
 
-    fclose(file);
+    outputFile.close();
+    inputFile.close();
 }
 
 std::string removeExtension(const std::string &fileName) {
@@ -100,62 +135,111 @@ std::string getModeName(const MODE mode) {
     return "error";
 }
 
-void x() {
-
-
-    int res;
-    unsigned char ot[1024] = "Text pro rc4.";  // open text
-    unsigned char st[1024];  // sifrovany text
-    unsigned char key[EVP_MAX_KEY_LENGTH] = "Muj klic";  // klic pro sifrovani
-    unsigned char iv[EVP_MAX_IV_LENGTH] = "inicial. vektor";  // inicializacni vektor
-    const char cipherName[] = "RC4";
-    const EVP_CIPHER *cipher;
-
-    OpenSSL_add_all_ciphers();
-    /* sifry i hashe by se nahraly pomoci OpenSSL_add_all_algorithms() */
-    cipher = EVP_get_cipherbyname(cipherName);
-    if (!cipher) {
-        printf("Sifra %s neexistuje.\n", cipherName);
-        exit(1);
+bool isFileBMP(char a, char b) {
+    if (a != 'B' && b != 'M') {
+        std::cout << "File is not a BMP." << std::endl;
+        return false;
     }
 
-    int otLength = strlen((const char *) ot);
-    int stLength = 0;
+    return true;
+}
+
+int process(std::fstream &inputFile, std::fstream &outputFile, MODE mode, METHOD method) {
+    int res;
+
+    unsigned char key[EVP_MAX_KEY_LENGTH] = KEY_VALUE;
+    unsigned char iv[EVP_MAX_IV_LENGTH] = IV_VALUE;
+
+    // Init cipher.
+    const EVP_CIPHER *cipher = mode == MODE::CBC ? EVP_des_cbc() : EVP_des_ecb();
+    if (!cipher) {
+        std::cout << "Cipher " << getModeName(mode) << " does not exist." << std::endl;
+        std::remove(outputFileName.c_str());
+        return 1;
+    }
+
+    // Init context.
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if (ctx == nullptr) {
+        std::remove(outputFileName.c_str());
+        return 2;
+    }
+    res = EVP_EncryptInit_ex(ctx, cipher, nullptr, key, iv);
+    if (res != 1) {
+        std::remove(outputFileName.c_str());
+        return 3;
+    }
+
+    char headerSizeData[4];
+    inputFile.seekg(10, std::fstream::beg);
+    inputFile.read(headerSizeData, 4);
+
+    long headerLength = 0;
+    for (int x = 0; x < 4; x++) {
+        headerLength += (unsigned long) headerSizeData[x] << (unsigned long) (8 * x);
+    }
+
+    inputFile.seekg(0, std::fstream::end);
+    long dataLength = inputFile.tellg() - headerLength;
+
+    // Contains data.
+    if (dataLength <= 0) {
+        std::cout << "Wrong data." << std::endl;
+        std::remove(outputFileName.c_str());
+        return 4;
+    }
+
+    char *headerData = new char[headerLength];
+
+    inputFile.seekg(0, std::fstream::beg);
+    inputFile.read(headerData, headerLength);
+
+    if (!isFileBMP(headerData[0], headerData[1])) {
+        std::cout << "Wrong BMP file." << std::endl;
+        std::remove(outputFileName.c_str());
+        return 4;
+    }
+
+    // Write header.
+    outputFile.write(headerData, headerLength);
+
+    // Store data.
+    inputFile.seekg(headerLength, std::fstream::beg);
+    char *data = new char[dataLength];
+    inputFile.read(data, dataLength);
+
+    // Prepare output data storage.
+    char *outputData = new char[((dataLength + 7) / 8) * 8];
+    long outputDataLength = 0;
+
     int tmpLength = 0;
 
-    EVP_CIPHER_CTX *ctx; // context structure
-    ctx = EVP_CIPHER_CTX_new();
-    if (ctx == NULL) exit(2);
+    res = EVP_CipherInit_ex(ctx, cipher, nullptr, key, iv, method == METHOD::ENCRYPT ? 1 : 0);
+    if (res != 1) {
+        std::cout << "Error in encryption." << std::endl;
+        std::remove(outputFileName.c_str());
+        return 5;
+    }
+    res = EVP_CipherUpdate(ctx, reinterpret_cast<unsigned char *>(outputData), &tmpLength,
+                            reinterpret_cast<const unsigned char *>(data), dataLength);
+    if (res != 1) {
+        std::cout << "Error in encryption." << std::endl;
+        std::remove(outputFileName.c_str());
+        return 6;
+    }
+    outputDataLength += tmpLength;
+    res = EVP_CipherFinal_ex(ctx, reinterpret_cast<unsigned char *>(outputData + outputDataLength), &tmpLength);
+    if (res != 1) {
+        std::cout << "Error in encryption." << std::endl;
+        std::remove(outputFileName.c_str());
+        return 7;
+    }
+    outputDataLength += tmpLength;
 
-    printf("OT: %s\n", ot);
+    // Write data.
+    outputFile.write(outputData, outputDataLength);
 
-    /* Sifrovani */
-    res = EVP_EncryptInit_ex(ctx, cipher, NULL, key, iv);  // context init - set cipher, key, init vector
-    if (res != 1) exit(3);
-    res = EVP_EncryptUpdate(ctx, st, &tmpLength, ot, otLength);  // encryption of pt
-    if (res != 1) exit(4);
-    stLength += tmpLength;
-    res = EVP_EncryptFinal_ex(ctx, st + stLength, &tmpLength);  // get the remaining ct
-    if (res != 1) exit(5);
-    stLength += tmpLength;
-
-    printf("Zasifrovano %d znaku.\n", stLength);
-
-    /* Desifrovani */
-    res = EVP_DecryptInit_ex(ctx, cipher, NULL, key, iv);  // nastaveni kontextu pro desifrovani
-    if (res != 1) exit(6);
-    res = EVP_DecryptUpdate(ctx, ot, &tmpLength, st, stLength);  // desifrovani st
-    if (res != 1) exit(7);
-    otLength += tmpLength;
-    res = EVP_DecryptFinal_ex(ctx, ot + otLength, &tmpLength);  // dokonceni (ziskani zbytku z kontextu)
-    if (res != 1) exit(8);
-    otLength += tmpLength;
-
-    /* Clean up */
+    // Clean up.
     EVP_CIPHER_CTX_free(ctx);
-
-    /* Vypsani zasifrovaneho a rozsifrovaneho textu. */
-    printf("ST: %s\nDT: %s\n", st, ot);
-
-    exit(0);
+    return 0;
 }
