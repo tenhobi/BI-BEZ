99cca2ecd8563999fdfa1b491bb237237258e759
Honza Bittner <git@honzabittner.cz>
2019-04-09 17:01:30 +0200

    Finish validation lab3

--- homeworks/lab-3/task.cpp
+++ homeworks/lab-3/task.cpp
@@ -22,20 +22,18 @@ enum class METHOD {
     DECRYPT
 };
 
-std::string outputFileName;
-
 std::string removeExtension(const std::string &fileName);
 
 std::string getModeName(MODE mode);
 
 bool isFileBMP(char a, char b);
 
-int process(std::fstream &inputFile, std::fstream &outputFile, MODE mode, METHOD method);
+int process(std::fstream &inputFile, std::string &outputFileName, MODE mode, METHOD method);
 
 int main(int argc, char *argv[]) {
     // Check number of parameters.
     if (argc != 4) {
-        std::cout << "Wrong number of arguments." << std::endl;
+        std::cout << "Error: Wrong number of arguments." << std::endl;
         return 1;
     }
 
@@ -62,62 +60,43 @@ int main(int argc, char *argv[]) {
 
     // Check content of parameters.
     if (task == TASK::NONE || mode == MODE::NONE) {
-        std::cout << "Wrong parameters." << std::endl;
+        std::cout << "Error: Wrong parameters." << std::endl;
         return 2;
     }
 
     // Read the input file.
     std::fstream inputFile;
     inputFile.open(sourceFile, std::ifstream::in | std::ifstream::binary);
+
     if (!inputFile.is_open()) {
-        std::cout << "Couldn't open the inputFile." << std::endl;
+        std::cout << "Error: Couldn't open the input file." << std::endl;
         return 3;
     }
 
-    std::fstream outputFile;
+    int returnCode = 0;
 
     // process
     if (task == TASK::ENCRYPT) {
-        outputFileName = removeExtension(sourceFile) + "_" + getModeName(mode) + ".bmp";
-        outputFile.open(outputFileName,
-                        std::ifstream::out | std::ifstream::binary);
-        if (!outputFile.is_open()) {
-            std::cout << "Couldn't open the output inputFile." << std::endl;
-            inputFile.close();
-            std::remove(outputFileName.c_str());
-            return 4;
-        }
+        std::string outputFileName = removeExtension(sourceFile) + "_" + getModeName(mode) + ".bmp";
 
-        if (process(inputFile, outputFile, mode, METHOD::ENCRYPT) < 0) {
-            std::cout << "Error while encrypting." << std::endl;
+        if ((returnCode = process(inputFile, outputFileName, mode, METHOD::ENCRYPT)) != 0) {
+            std::cout << "Error: Error while encrypting." << std::endl;
             inputFile.close();
-            outputFile.close();
-            std::remove(outputFileName.c_str());
-            return 5;
+            return returnCode;
         }
     } // decrypt
     else {
-        std::fstream outputFile;
-        outputFileName = removeExtension(sourceFile) + "_dec.bmp";
-        outputFile.open(outputFileName, std::ifstream::out | std::ifstream::binary);
-        if (!outputFile.is_open()) {
-            std::cout << "Couldn't open the output inputFile." << std::endl;
-            inputFile.close();
-            std::remove(outputFileName.c_str());
-            return 4;
-        }
+        std::string outputFileName = removeExtension(sourceFile) + "_dec.bmp";
 
-        if (process(inputFile, outputFile, mode, METHOD::DECRYPT) < 0) {
+        if ((returnCode = process(inputFile, outputFileName, mode, METHOD::DECRYPT)) != 0) {
             std::cout << "Error while decrypting." << std::endl;
             inputFile.close();
-            outputFile.close();
-            std::remove(outputFileName.c_str());
-            return 5;
+            return returnCode;
         }
     }
 
-    outputFile.close();
     inputFile.close();
+    std::cout << "Success." << std::endl;
 }
 
 std::string removeExtension(const std::string &fileName) {
@@ -144,7 +123,7 @@ bool isFileBMP(char a, char b) {
     return true;
 }
 
-int process(std::fstream &inputFile, std::fstream &outputFile, MODE mode, METHOD method) {
+int process(std::fstream &inputFile, std::string &outputFileName, MODE mode, METHOD method) {
     int res;
 
     unsigned char key[EVP_MAX_KEY_LENGTH] = KEY_VALUE;
@@ -153,90 +132,106 @@ int process(std::fstream &inputFile, std::fstream &outputFile, MODE mode, METHOD
     // Init cipher.
     const EVP_CIPHER *cipher = mode == MODE::CBC ? EVP_des_cbc() : EVP_des_ecb();
     if (!cipher) {
-        std::cout << "Cipher " << getModeName(mode) << " does not exist." << std::endl;
-        std::remove(outputFileName.c_str());
-        return 1;
+        std::cout << "Error: Cipher " << getModeName(mode) << " does not exist." << std::endl;
+        return 4;
     }
 
     // Init context.
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
     if (ctx == nullptr) {
-        std::remove(outputFileName.c_str());
-        return 2;
+        return 5;
     }
     res = EVP_EncryptInit_ex(ctx, cipher, nullptr, key, iv);
     if (res != 1) {
-        std::remove(outputFileName.c_str());
-        return 3;
+        return 6;
     }
 
+    // Load file size data.
+    char fileSizeData[4];
+    inputFile.seekg(2, std::fstream::beg);
+    inputFile.read(fileSizeData, 4);
+    long fileLength = 0;
+    for (int x = 0; x < 4; x++) {
+        fileLength += (unsigned long) fileSizeData[x] << (unsigned long) (8 * x);
+    }
+
+    // Load header size data.
     char headerSizeData[4];
     inputFile.seekg(10, std::fstream::beg);
     inputFile.read(headerSizeData, 4);
-
     long headerLength = 0;
     for (int x = 0; x < 4; x++) {
         headerLength += (unsigned long) headerSizeData[x] << (unsigned long) (8 * x);
     }
 
+    // Compute data length.
     inputFile.seekg(0, std::fstream::end);
     long dataLength = inputFile.tellg() - headerLength;
 
-    // Contains data.
+    // Check if data exists.
     if (dataLength <= 0) {
-        std::cout << "Wrong data." << std::endl;
-        std::remove(outputFileName.c_str());
-        return 4;
+        std::cout << "Error: wrong data length." << std::endl;
+        return 7;
     }
 
-    char *headerData = new char[headerLength];
+    // Check if the data length is ok.
+    if (dataLength + headerLength != fileLength) {
+        std::cout << "Error: wrong file length" << std::endl;
+        return 8;
+    }
 
+    // Check for BMP file format.
+    char *headerData = new char[headerLength];
     inputFile.seekg(0, std::fstream::beg);
     inputFile.read(headerData, headerLength);
-
     if (!isFileBMP(headerData[0], headerData[1])) {
-        std::cout << "Wrong BMP file." << std::endl;
-        std::remove(outputFileName.c_str());
-        return 4;
+        std::cout << "Error: wrong BMP file format." << std::endl;
+        return 9;
     }
 
-    // Write header.
-    outputFile.write(headerData, headerLength);
-
-    // Store data.
+    // Store input data.
     inputFile.seekg(headerLength, std::fstream::beg);
     char *data = new char[dataLength];
     inputFile.read(data, dataLength);
 
     // Prepare output data storage.
-    char *outputData = new char[((dataLength + 7) / 8) * 8];
+    char *outputData = new char[dataLength];
     long outputDataLength = 0;
-
     int tmpLength = 0;
 
     res = EVP_CipherInit_ex(ctx, cipher, nullptr, key, iv, method == METHOD::ENCRYPT ? 1 : 0);
     if (res != 1) {
         std::cout << "Error in encryption." << std::endl;
-        std::remove(outputFileName.c_str());
-        return 5;
+        return 11;
     }
     res = EVP_CipherUpdate(ctx, reinterpret_cast<unsigned char *>(outputData), &tmpLength,
                             reinterpret_cast<const unsigned char *>(data), dataLength);
     if (res != 1) {
         std::cout << "Error in encryption." << std::endl;
-        std::remove(outputFileName.c_str());
-        return 6;
+        return 12;
     }
     outputDataLength += tmpLength;
     res = EVP_CipherFinal_ex(ctx, reinterpret_cast<unsigned char *>(outputData + outputDataLength), &tmpLength);
     if (res != 1) {
         std::cout << "Error in encryption." << std::endl;
-        std::remove(outputFileName.c_str());
-        return 7;
+        return 13;
     }
     outputDataLength += tmpLength;
 
-    // Write data.
+    // Everything is OK, open outputFile.
+    std::fstream outputFile;
+    outputFile.open(outputFileName, std::ifstream::out | std::ifstream::binary);
+
+    if (!outputFile.is_open()) {
+        std::cout << "Error: Couldn't open the output file." << std::endl;
+        return 10;
+    }
+
+    // Write header data.
+    headerData[2] = outputDataLength + headerLength;
+    outputFile.write(headerData, headerLength);
+
+    // Write data data.
     outputFile.write(outputData, outputDataLength);
 
     // Clean up.
